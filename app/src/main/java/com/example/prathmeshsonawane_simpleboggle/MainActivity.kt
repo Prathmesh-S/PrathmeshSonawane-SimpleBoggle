a045fea96a5669e2de554e090e3fb5e70412295f
PrathmeshS <prs.lowell@gmail.com>
2024-04-01 22:56:17 -0400

    Finished application. Added extra credit shake to reset Game!

--- app/src/main/java/com/example/prathmeshsonawane_simpleboggle/MainActivity.kt
+++ app/src/main/java/com/example/prathmeshsonawane_simpleboggle/MainActivity.kt
@@ -10,6 +10,10 @@ import android.hardware.SensorManager
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity(), ScoreFragment.OnNewGameRequestedListener, GameFragment.OnScoreUpdateListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var shakeDetector: ShakeDetector? = null
+    private var accelerometer: Sensor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +30,20 @@ class MainActivity : AppCompatActivity(), ScoreFragment.OnNewGameRequestedListen
             }
         }
 
+        val shakeDetector = ShakeDetector {
+            onScoreUpdate(0)
+            onNewGameRequested()
+        }
+        // Get the system service for SensorManager
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+// Get the accelerometer sensor
+        val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+// Register the ShakeDetector as a listener for accelerometer sensor events
+        sensorManager.registerListener(shakeDetector, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+
+
     }
 
     override fun onNewGameRequested() {
@@ -37,4 +55,37 @@ class MainActivity : AppCompatActivity(), ScoreFragment.OnNewGameRequestedListen
         val scoreFragment = supportFragmentManager.findFragmentById(R.id.scoreContainer) as? ScoreFragment
         scoreFragment?.newScore(score)
     }
+
+    //Extra Credit:
+
+    private class ShakeDetector(private val onShake: () -> Unit) : SensorEventListener {
+        private var lastTime: Long = 0
+        private var lastX: Float = 0.0f
+        private var lastY: Float = 0.0f
+        private var lastZ: Float = 0.0f
+        private val shakeThreshold = 400
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        }
+
+        override fun onSensorChanged(event: SensorEvent?) {
+            val currentTime = System.currentTimeMillis()
+            if ((currentTime - lastTime) > 100) {
+                val x = event?.values?.get(0) ?: 0f
+                val y = event?.values?.get(1) ?: 0f
+                val z = event?.values?.get(2) ?: 0f
+
+                val speed = Math.sqrt(((x - lastX).toDouble().pow(2.0) + (y - lastY).toDouble().pow(2.0) + (z - lastZ).toDouble().pow(2.0))) / (currentTime - lastTime) * 10000
+                if (speed > shakeThreshold) {
+                    onShake()
+                }
+
+                lastTime = currentTime
+                lastX = x
+                lastY = y
+                lastZ = z
+            }
+        }
+    }
+
 }
